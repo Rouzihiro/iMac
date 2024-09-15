dd7a8d9ae04a3fe2a560b435cd9b6c701966c8ac
Rouzihiro <ryossj@gmail.com>
2024-09-15 06:40:09 -0500

    home manager modification: kitty opacity

--- hosts/Mac/home.nix
+++ hosts/Mac/home.nix
@@ -177,6 +177,7 @@ in
         wheel_scroll_min_lines = 1;
         window_padding_width = 4;
         confirm_os_window_close = 0;
+        background_opacity = 0.5;
       };
       extraConfig = ''
         tab_bar_style fade
