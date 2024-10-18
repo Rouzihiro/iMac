2d1f1fbc0864afb631935838bd69b494cff51946
Rouzihiro <ryossj@gmail.com>
2024-10-18 18:05:50 +0200

    modifying home.nix to add extract function

--- hosts/Mac/home.nix
+++ hosts/Mac/home.nix
@@ -9,7 +9,7 @@
 let
   inherit (import ./variables.nix) gitUsername gitEmail;
  # dots = "/dotfiles";
-  scripts = "/home/${username}/scripts";
+  scripts = "/home/${username}/zaneyos/scripts";
 in
 {
   # Home Manager Settings
@@ -240,6 +240,51 @@ in
         psg = "ps aux | grep";
         rey-weatherHH = "curl -4 http://wttr.in/Hamburg";
         rey-weather = "curl -4 http://wttr.in/";
+
+ extract = ''
+
+          () {
+
+            if [ -f $1 ] ; then
+
+              case $1 in
+
+                *.tar.bz2)   tar xvjf $1    ;;
+
+                *.tar.gz)    tar xvzf $1    ;;
+
+                *.tar.xz)    tar Jxvf $1    ;;
+
+                *.bz2)       bunzip2 $1     ;;
+
+                *.rar)       rar x $1       ;;
+
+                *.gz)        gunzip $1      ;;
+
+                *.tar)       tar xvf $1     ;;
+
+                *.tbz2)      tar xvjf $1    ;;
+
+                *.tgz)       tar xvzf $1    ;;
+
+                *.zip)       unzip -d `echo $1 | sed 's/\(.*\)\.zip/\1/'` $1;;
+
+                *.Z)         uncompress $1  ;;
+
+                *.7z)        7z x $1        ;;
+
+                *)           echo "don't know how to extract '$1'" ;;
+
+              esac
+
+            else
+
+              echo "'$1' is not a valid file!"
+
+            fi
+
+          }'';
+
       };
  history = {
     size = 10000;
